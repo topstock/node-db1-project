f9531da1bef22c615ad5a3a809f05082b3d280f8
Phil G <fillupgalbraith@gmail.com>
2022-01-18 13:21:14 -0800

    testing connections to router

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -2,26 +2,55 @@ const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    throw new Error('argh!')
+  } catch(err) {
+    next({ status: 422, message: 'this is horrible'})
+  }
+  
 })
 
 router.get('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+
+  } catch(err){
+    next(err)
+  }
 })
 
 router.post('/', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+
+  } catch(err){
+    next(err)
+  }
 })
 
 router.put('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+
+  } catch(err){
+    next(err)
+  }
 });
 
 router.delete('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+
+  } catch(err){
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    message: err.message
+  })
 })
 
 module.exports = router;
